1b359da39976a0b3c765dfd75521cee2c9e53f70
Ochiere <144838836+Ochiere@users.noreply.github.com>
2024-02-13 14:59:41 +0300

    Add files via upload

---
+++ Simple calculator assign 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//Simple addition and subtraction calculator
+int main()
+{  
+    double a,b,sum,diff;        
+    printf("Enter the two numbers of your choice. \n");
+    scanf("%lf %lf", &a, &b);
+    sum=a+b;
+    diff=a-b;
+    printf("Summation is %f\n",sum);
+    printf("Difference is %f\n",diff);
+    printf("Summation and Difference are:%f\t %f\t", sum, diff);
+    return 0;
+}
